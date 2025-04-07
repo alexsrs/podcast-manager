e76a3e19162088f4eb43c4b18ab7e564508c811e
Alex Sandro <alex@creapix.com.br>
2025-04-07 11:10:12 -0300

    feat: create routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import * as http from 'http';
-import {getListEpisodes} from "./controllers/podcasts-controller";
+import {getListEpisodes, getFilterEpisodes} from "./controllers/podcasts-controller";
 
 
 const server = http.createServer(
     async (req: http.IncomingMessage, res: http.ServerResponse) => {
-        if(req.method === "GET"){
-           await getListEpisodes(req, res);
+        if(req.method === "GET" && req.url === "/api/list") {
+            await getListEpisodes(req, res);
+        }
+
+        if(req.method === "GET" && req.url === "/api/filter") {
+            await getFilterEpisodes(req, res);
         }
     }
 );
